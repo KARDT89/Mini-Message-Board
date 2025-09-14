a3c45c529eb9f1632d6235e23e67e3ee6c003cb4
DT89-lappy <dt89@rockzz.com>
2025-09-14 21:52:11 +0530

    add: form func

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const messageRouter = require("./routes/messages.routes.js")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use("/", messageRouter)
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
--- routes/messages.routes.js
+++ routes/messages.routes.js
@@ -1,22 +1,33 @@
-
 const { Router } = require("express");
 
 const messageRouter = Router();
 
 const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
 ];
 
+messageRouter.get("/", (req, res) => {
+    res.render("index", { title: "Mini Messageboard", messages: messages });
+});
 
-messageRouter.get("/")
+messageRouter.get("/new", (req, res) => {
+    res.render("form.ejs");
+});
+
+messageRouter.post("/new", (req, res) => {
+    const { authorName, messageText } = req.body;
+    messages.push({ text: messageText, user: authorName, added: new Date() });
+    res.redirect("/")
+    
+});
 
 module.exports = messageRouter;
