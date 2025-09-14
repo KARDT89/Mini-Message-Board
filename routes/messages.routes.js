4534bad5d7cc87b411ac49a746565682b7eac0c1
dt89 <tamalsarkartwoooo@gmail.com>
2025-09-14 15:40:44 +0000

    init

---
+++ routes/messages.routes.js
@@ -0,0 +1,22 @@
+
+const { Router } = require("express");
+
+const messageRouter = Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
+messageRouter.get("/")
+
+module.exports = messageRouter;
